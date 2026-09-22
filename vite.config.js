5a66a40090ba3978e3723de89f180e6ce0fb77a8
Carla-guguwwwy051010 <yanwang15790776162@gmail.com>
2026-09-22 14:40:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/ai_product_governance/",
   plugins: [react()],
-  base: '/ai_product_governance/',
 })
